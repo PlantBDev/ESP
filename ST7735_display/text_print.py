415895471b38455f17e3dea2bbbce07d1b5986b9
Kalle-Lassila <kalle.j.lassila@protonmail.com>
2021-03-23 17:39:01 +0200

    Modified text_print to be callable

--- ST7735_display/text_print.py
+++ ST7735_display/text_print.py
@@ -1,42 +1,43 @@
 #A python script that displays text on the TFT-lcd
 
-import digitalio
-import board
-from PIL import Image, ImageDraw, ImageFont
-import adafruit_rgb_display.st7735 as st7735
-BORDER = 20
-FONTSIZE = 24
+def print():
+    import digitalio
+    import board
+    from PIL import Image, ImageDraw, ImageFont
+    import adafruit_rgb_display.st7735 as st7735
+    BORDER = 20
+    FONTSIZE = 24
 
-#pins for SPI communication and reset
-cs_pin = digitalio.DigitalInOut(board.CE0)
-dc_pin = digitalio.DigitalInOut(board.D25)
-reset_pin = digitalio.DigitalInOut(board.D24)
-BAUDRATE = 24000000
+    #pins for SPI communication and reset
+    cs_pin = digitalio.DigitalInOut(board.CE0)
+    dc_pin = digitalio.DigitalInOut(board.D25)
+    reset_pin = digitalio.DigitalInOut(board.D24)
+    BAUDRATE = 24000000
 
-spi = board.SPI()
+    spi = board.SPI()
 
-disp = st7735.ST7735R(spi, rotation=90,
-    cs=cs_pin,
-    dc=dc_pin,
-    rst=reset_pin,
-    baudrate=BAUDRATE,
-)
+    disp = st7735.ST7735R(spi, rotation=90,
+        cs=cs_pin,
+        dc=dc_pin,
+        rst=reset_pin,
+        baudrate=BAUDRATE,
+    )
 
 
-height = disp.width
-width = disp.height
+    height = disp.width
+    width = disp.height
 
-image = Image.new("RGB", (width, height))
-draw = ImageDraw.Draw(image)
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", FONTSIZE)
+    image = Image.new("RGB", (width, height))
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", FONTSIZE)
 
-text = "Testi Testi"
-(font_width, font_height) = font.getsize(text)
-draw.text(
-    (width // 2 - font_width // 2, height // 2 - font_height // 2),
-    text,
-    font=font,
-    fill=(255, 255, 0),
-)
+    text = "Testi Testi"
+    (font_width, font_height) = font.getsize(text)
+    draw.text(
+        (width // 2 - font_width // 2, height // 2 - font_height // 2),
+        text,
+        font=font,
+        fill=(255, 255, 0),
+    )
 
-disp.image(image)
+    disp.image(image)
---
+++ ST7735_display/text_print_test.py
@@ -0,0 +1,4 @@
+
+#a test script for the text_print script
+import text_print
+text_print.print()
