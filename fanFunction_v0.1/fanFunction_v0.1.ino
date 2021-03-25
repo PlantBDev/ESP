4e193d4838a3e48999cb29c34c41a692186e684f
5uori5 <69713809+5uori5@users.noreply.github.com>
2021-03-25 10:51:21 +0200

    Update fanFunction_v0.1.ino

--- fanFunction_v0.1/fanFunction_v0.1.ino
+++ fanFunction_v0.1/fanFunction_v0.1.ino
@@ -3,11 +3,11 @@
 void fan(); //The name of the function
 int relay_pin = 9; //Pin that controls the fan, if it's high fan is ON and if it's low, the fan is OFF
 int fanTime = 2000; //Wanted time the fan is on (will be added to MAIN later)
-pinMode(relay_pin, OUTPUT);
-digitalWrite(relay_pin, LOW); //Fan is off by default
 
 void setup() { 
+  pinMode(relay_pin, OUTPUT);
   Serial.begin(9600); // initialize serial communications at 9600 bps:
+  digitalWrite(relay_pin, LOW); //Fan is off by default
 }
 
 void loop() { 
@@ -18,6 +18,7 @@ void fan(){
   digitalWrite(relay_pin, HIGH);
   delay(fanTime); //The wanted time that the fan is on
   digitalWrite(relay_pin, LOW);
+  delay(fanTime);
 #if DEBUG
 // Print the state of the fan to the Serial Monitor:
   if (relay_pin == HIGH){
@@ -25,7 +26,7 @@ void fan(){
     delay (10);
 }
   else {
-    Serial.println("Fan is OFF ");
+    Serial.println("The fan is OFF");
     delay (10);
 }
 #endif
